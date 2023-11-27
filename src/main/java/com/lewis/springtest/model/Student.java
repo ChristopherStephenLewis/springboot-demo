e8583501a4954496ee1d1da4070cc9f9d0842803
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-11-27 17:30:34 -0500

    Set up Student as an entity for the database

--- src/main/java/com/lewis/springtest/model/Student.java
+++ src/main/java/com/lewis/springtest/model/Student.java
@@ -1,5 +1,6 @@
 package com.lewis.springtest.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,19 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
