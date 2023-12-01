ffc9c4f11808c8e8dfcf03318b8cc943a5aac5a0
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-11-30 21:11:17 -0500

    Implemented addStudent POST request

--- src/main/java/com/lewis/springtest/model/StudentService.java
+++ src/main/java/com/lewis/springtest/model/StudentService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 // Business/Service Layer
 @Service
@@ -21,5 +22,13 @@ public class StudentService {
 
     public List<Student> getStudents() {
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        if (studentOptional.isPresent()) {
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
