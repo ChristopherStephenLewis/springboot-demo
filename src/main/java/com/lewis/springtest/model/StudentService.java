3106fc90df68a676e5f6c69cc38a08908a580021
Christopher Lewis <chris.stephen.lewis@gmail.com>
2023-12-03 17:12:50 -0500

    Added Put Mapping

--- src/main/java/com/lewis/springtest/model/StudentService.java
+++ src/main/java/com/lewis/springtest/model/StudentService.java
@@ -2,10 +2,12 @@ package com.lewis.springtest.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 // Business/Service Layer
@@ -38,5 +40,24 @@ public class StudentService {
             throw new IllegalStateException("student with id " + studentId + " does not exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional // with this we don't use queries (entity goes into a managed state)
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "student with id " + studentId + " does not exist"));
+
+        if (name != null && !name.isEmpty() && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && !email.isEmpty() && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
